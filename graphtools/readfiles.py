72d5eee52237b995cecf3776a626ccb9eb3f022f
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2020-05-26 16:18:10 +0200

    read all precomputed subjects in regina's folder

--- graphtools/readfiles.py
+++ graphtools/readfiles.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import glob
 import pandas_profiling
 
 #notes_path = '/home/skapoor/Thesis/Notes/HCP_data'
@@ -26,6 +27,30 @@ def computed_subjects():
                 present_subj.append(subject)
     data = df.loc[df['Subject'].isin(present_subj), :]  # reduced csv files containing data of only computed subj
     return data
+
+def precomputed_subjects():
+    '''
+
+    :param notes_path: the notes directory
+    :param input_dir: the input directory containing the results that have been pre-processed
+    :param edge_file: the connectome files that we have already computed
+    :return data: the data of only the subjects whose data has been pre-processed
+    '''
+    notes_path = '/home/skapoor/Thesis/Notes/HCP_data'
+    input_dir = '/data/regina/HCP'
+    df = pd.read_csv(f'{notes_path}/unrestricted_mdkhatami_3_2_2017_5_48_20.csv')
+    present_subj = []
+    #Now we need to check for which all subjects the meam_FA_connectome exists!
+    for s in glob.glob(f'{input_dir}/*/T1w/Diffusion/'):
+        if s.split('/HCP/')[1][0] in ['1', '2']:
+            #print(s)
+            subject = s.split('/HCP/')[1].split('/')[0]
+            #print(subject)
+            if int(subject) in np.array(df['Subject']):
+                present_subj.append(subject)
+    data = df.loc[df['Subject'].isin(present_subj), :]  # reduced csv files containing data of only computed subj
+    return data
+
 
 def make_profiles():
     '''
