83e3f68faa9a7ead8b264535d1ee4d5103dfa9c6
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2020-05-26 16:17:37 +0200

    big5 report and information

--- graphtools/big5personality.py
+++ graphtools/big5personality.py
@@ -3,24 +3,12 @@ import pandas as pd
 import os
 import glob
 import numpy as np
+from readfiles import computed_subjects
+from readfiles import precomputed_subjects
 #%%
 print(os.getcwd())
 #%%
-df = pd.read_csv('/home/skapoor/Thesis/Notes/HCP_data/unrestricted_mdkhatami_3_2_2017_5_48_20.csv')
-input_dir = '/data/skapoor/HCP/results'
-#%%
-present_subj = []
-for s in glob.glob(f'{input_dir}/*/T1w/Diffusion/mean_FA_connectome_5M.csv'):
-    if s.split('/HCP/results/')[1][0] in ['1', '2']:
-        #print(s)
-        subject = s.split('/HCP/results/')[1].split('/')[0]
-        #print(subject)
-        if int(subject) in np.array(df['Subject']):
-            #print(subject, ' present in file')
-            #print(df.loc[df['Subject']==int(subject), :])
-            present_subj.append(subject)
-#%%
-data = df.loc[df['Subject'].isin(present_subj), :] #reduced csv files containing data of only computed subj
+data = computed_subjects()
 data.to_csv('present_subjects.csv')
 #%%
 '''
@@ -35,20 +23,30 @@ info = pd.read_excel('/home/skapoor/Thesis/Notes/HCP_data/HCP_S1200_DataDictiona
 info.head()
 #%%
 labels = ['NEOFAC_A', 'NEOFAC_O', 'NEOFAC_C', 'NEOFAC_N', 'NEOFAC_E' ]
-labels = ['NEOFAC_A', 'NEOFAC_O', 'NEOFAC_C', 'NEOFAC_N', 'NEOFAC_E' ]
 #%%
 info = info.set_index('columnHeader')
-#%%
 info.loc[labels]['description']
-#%%
 # Extract these labels for our subjects!
 big5 = data.loc[:, labels]
-#%%
 big5.describe()
-#%%
 import pandas_profiling
-#%%
 profile = big5.profile_report()
 #%%
 #profile = pandas_profiling.ProfileReport(big5)
 profile.to_file('./report-big5.html')
+#%%
+data = precomputed_subjects() #reduced csv files containing target values from regina's folder
+data.to_csv('present_subjects_regina.csv')
+# cut and put it into three categories, namely low medium and high
+agreeable = pd.Series(pd.cut(data['NEOFAC_A'],3,  labels=False, retbins=True, right=False)[0])
+open =pd.Series(pd.cut(data['NEOFAC_O'],3,  labels=False, retbins=True, right=False)[0])
+consci = pd.Series(pd.cut(data['NEOFAC_C'],3,  labels=False, retbins=True, right=False)[0])
+neuroti = pd.Series(pd.cut(data['NEOFAC_N'],3,  labels=False, retbins=True, right=False)[0])
+extrav = pd.Series(pd.cut(data['NEOFAC_E'],3,  labels=False, retbins=True, right=False)[0])
+#%%
+#how balanced are these cuts?
+big5 = pd.concat([agreeable, open, consci, neuroti, extrav], axis=1)
+big5.columns = ['Agreeableness', 'Openness', 'Conscientiousness', 'Neuroticism',
+                            'Extraversion']
+binned_traits = big5.profile_report()
+binned_traits.to_file('./big5-binned.html')
