42012905c12ffa1559a615e4cd048ac3d9f9f824
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2020-05-26 15:41:56 +0200

    big5

--- graphtools/classifier_3labels_flatten.py
+++ graphtools/classifier_3labels_flatten.py
@@ -16,7 +16,7 @@ def get_subj_ids():
     return(present_subj)
 #%%
 
-whole = np.zeros((len(get_subj_ids()),3,7056))
+whole = np.zeros((len(get_subj_ids()),7056*3))
 j =0
 '''
 There are three features that we want to add to the files
@@ -24,6 +24,8 @@ There are three features that we want to add to the files
 2. The mean length of the streamlines between the two nodes
 3. The number of streamlines between the two nodes
 '''
+
+
 for subject in get_subj_ids():
     out_diff = f'/data/skapoor/HCP/results/{subject}/T1w/Diffusion'
     files = [f'{out_diff}/mean_FA_connectome_1M_SIFT.csv', f'{out_diff}/distances_mean_1M_SIFT.csv',
@@ -35,7 +37,7 @@ for subject in get_subj_ids():
         edge_feature = np.array(pd.read_csv(file, sep =' ', header= None))
     # the file shall be number of subjects x 7056
         edge_feature = np.reshape(edge_feature, (7056,))
-        whole[j,i,:] = edge_feature
+        whole[j,i*7056:(i+1)*7056] = edge_feature
         #print(i,j)
         i+=1
     j+=1
@@ -52,16 +54,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import Normalizer
 norm = Normalizer()
 #%%
-correlations = np.zeros((7056, 3,3))
-# pearson correlation per edge, lets try for one edge first
-for j in range(7056):
-    scaler =StandardScaler()
-    edge = pd.DataFrame(whole[:,:,j], columns=['mean FA', 'mean strl length', 'num strl'])
-    #print(edge.head())
-    #print(edge.corr())
-    edge = pd.DataFrame(norm.fit_transform(edge), columns=['mean FA', 'mean strl length', 'num strl'])
-    #print(edge.head())
-    correlations[j,:,:] = edge.corr()
+from sklearn.model_selection import train_test_split
+import numpy as np
 #%%
 '''
 5 personality traits that we want to study are:
@@ -78,5 +72,37 @@ open = np.array(pd.cut(data['NEOFAC_O'],3,  labels=False, retbins=True, right=Fa
 consci = np.array(pd.cut(data['NEOFAC_C'],3,  labels=False, retbins=True, right=False)[0])
 neuroti = np.array(pd.cut(data['NEOFAC_N'],3,  labels=False, retbins=True, right=False)[0])
 extrav = np.array(pd.cut(data['NEOFAC_E'],3,  labels=False, retbins=True, right=False)[0])
-
 #%%
+#how balanced are these cuts?
+pd.concat([agreeable, open, consci, neuroti, extrav],  axis=1)
+#%%
+Y = agreeable
+X = whole
+X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.80)
+#%%
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+clf.fit(X_train, y_train)
+#%%
+y_pred = clf.predict(X_test)
+#%%
+acc = sum(y_pred==y_test)/len(y_test)
+#%%
+print(acc)
+y_pred
+#%%
+sum(data['Gender']=='M')/68
+#%%
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+X, y = make_classification(n_samples= len(data), n_features=len(data.columns),
+                           n_informative=2, n_redundant=0,
+                           random_state=0, shuffle=False)
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf.fit(X_train, y_train)
+#%%
+y_pred = clf.predict(X_test)
+acc = sum(y_pred==y_test)/len(y_test)
+print(acc)
