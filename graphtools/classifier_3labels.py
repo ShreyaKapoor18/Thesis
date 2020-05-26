f24dfa1abae44540e85454f0ef9e59d37b78701f
ShreyaKapoor18 <sk180797>
2020-05-26 12:34:57 +0200

    edge correlation

--- graphtools/classifier_3labels.py
+++ graphtools/classifier_3labels.py
@@ -16,7 +16,7 @@ def get_subj_ids():
     return(present_subj)
 #%%
 
-whole = np.zeros((len(get_subj_ids()),7056))
+whole = np.zeros((len(get_subj_ids()),3,7056))
 j =0
 '''
 There are three features that we want to add to the files
@@ -24,59 +24,32 @@ There are three features that we want to add to the files
 2. The mean length of the streamlines between the two nodes
 3. The number of streamlines between the two nodes
 '''
-files = [f'{out_diff}/mean_FA_connectome_1M_SIFT.csv',f'{out_diff}/distances_mean_1M_SIFT.csv',
-         f'{out_diff}/connectome_1M.csv']
+
 
 for subject in get_subj_ids():
     out_diff = f'/data/skapoor/HCP/results/{subject}/T1w/Diffusion'
-
-    file = f'{out_diff}/mean_FA_connectome_1M_SIFT.csv'
+    files = [f'{out_diff}/mean_FA_connectome_1M_SIFT.csv', f'{out_diff}/distances_mean_1M_SIFT.csv',
+             f'{out_diff}/connectome_1M.csv']
+    i = 0
+    for file in files:
         #print(file) # we need to make all edges as a feature for each subject!
         # so we will have 84x84 features
-    mean_FA = np.array(pd.read_csv(file, sep = ' ', header= None))
+        edge_feature = np.array(pd.read_csv(file, sep =' ', header= None))
     # the file shall be number of subjects x 7056
-    mean_FA = np.reshape(mean_FA, (7056,))
-    whole[j,:] = mean_FA
+        edge_feature = np.reshape(edge_feature, (7056,))
+        whole[j,i,:] = edge_feature
+        print(i,j)
+        i+=1
+    j+=1
 #%%
 np.shape(whole[0])
 #%%
 # Let us take the labels i.e. the ones from unrestricted_files!
 data = pd.read_csv('present_subjects.csv')
 #%%
-print(data['Gender'].shape)
+print(data['NEOFAC_A'].shape)
 print(whole.shape)
 #%%
-from sklearn.model_selection import train_test_split
-import numpy as np
-#%%
-
-Y = np.array(data['Gender'])
-X = whole
-X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.80)
-#%%
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
-clf.fit(X_train, y_train)
-#%%
-y_pred = clf.predict(X_test)
-#%%
-acc = sum(y_pred==y_test)/len(y_test)
-#%%
-print(acc)
-y_pred
-#%%
-sum(data['Gender']=='M')/68
-#%%
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-X, y = make_classification(n_samples= len(data), n_features=len(data.columns),
-                           n_informative=2, n_redundant=0,
-                           random_state=0, shuffle=False)
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(X_train, y_train)
-#%%
-y_pred = clf.predict(X_test)
-acc = sum(y_pred==y_test)/len(y_test)
-print(acc)
+# pearson correlation per edge, lets try for one edge first
+edge = pd.DataFrame(whole[:,:,100])
+edge.corr()
