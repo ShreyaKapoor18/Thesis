390beae28c740c1c53b3ab22b3aae351b5789bfa
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2020-05-26 16:18:57 +0200

    flattened version edges duplicated but with different infos from files

--- graphtools/classifier_3labels_flatten.py
+++ graphtools/classifier_3labels_flatten.py
@@ -2,13 +2,13 @@ import glob
 import numpy as np
 import pandas as pd
 
-
+#checking in reginas folder if the subjects we have the labels for them are balanced or not
 def get_subj_ids():
     input_dir = '/data/skapoor/HCP/results'
     present_subj = []
     #Now we need to check for which all subjects the meam_FA_connectome exists!
     #we get this 1M file containing most biologically important features
-    for s in glob.glob(f'{input_dir}/*/T1w/Diffusion/mean_FA_connectome_1M_SIFT.csv'):
+    for s in glob.glob(f'{input_dir}/*/T1w/Diffusion/'):
         if s.split('/HCP/results/')[1][0] in ['1', '2']:
             #print(s)
             subject = s.split('/HCP/results/')[1].split('/')[0]
@@ -27,7 +27,7 @@ There are three features that we want to add to the files
 
 
 for subject in get_subj_ids():
-    out_diff = f'/data/skapoor/HCP/results/{subject}/T1w/Diffusion'
+    out_diff = f'/data/regina/HCP/{subject}/T1w/Diffusion'
     files = [f'{out_diff}/mean_FA_connectome_1M_SIFT.csv', f'{out_diff}/distances_mean_1M_SIFT.csv',
              f'{out_diff}/connectome_1M.csv']
     i = 0
@@ -55,28 +55,9 @@ from sklearn.preprocessing import Normalizer
 norm = Normalizer()
 #%%
 from sklearn.model_selection import train_test_split
-import numpy as np
+
 #%%
-'''
-5 personality traits that we want to study are:
-NEO-FFI Agreeableness (NEOFAC_A)
-NEO-FFI Openness to Experience (NEOFAC_O)
-NEO-FFI Conscientiousness (NEOFAC_C)
-NEO-FFI Neuroticism (NEOFAC_N)
-NEO-FFI Extraversion (NEOFAC_E)
-'''
-#%%
-# cut and put it into three categories, namely low medium and high
-agreeable = np.array(pd.cut(data['NEOFAC_A'],3,  labels=False, retbins=True, right=False)[0])
-open = np.array(pd.cut(data['NEOFAC_O'],3,  labels=False, retbins=True, right=False)[0])
-consci = np.array(pd.cut(data['NEOFAC_C'],3,  labels=False, retbins=True, right=False)[0])
-neuroti = np.array(pd.cut(data['NEOFAC_N'],3,  labels=False, retbins=True, right=False)[0])
-extrav = np.array(pd.cut(data['NEOFAC_E'],3,  labels=False, retbins=True, right=False)[0])
-#%%
-#how balanced are these cuts?
-pd.concat([agreeable, open, consci, neuroti, extrav],  axis=1)
-#%%
-Y = agreeable
+Y = data['NEOFAC_A']
 X = whole
 X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.80)
 #%%
