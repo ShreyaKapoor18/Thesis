207893aadc1c8bf5c0dd20eeecb887de64044ab2
Shreya Kapoor <kapoorshreya18@gmail.com>
2020-06-03 15:59:17 +0200

    main file for training from graph

--- graphtools/classifier_3labels_flatten.py
+++ graphtools/classifier_3labels_flatten.py
@@ -1,102 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.pipeline import make_pipeline
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import Normalizer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
+from train_classifier import train_SVC, train_RandomForest
 from sklearn.model_selection import train_test_split
-from readfiles import get_subj_ids
 from readfiles import computed_subjects
-import matplotlib.pyplot as plt
-from metrics import fscore
-from scipy import stats
-#%%
-def generate_combined_matrix(tri):
-    '''
-    There are three features that we want to add to the matrix for all subjects
-    1. Mean FA between the two nodes
-    2. The mean length of the streamlines between the two nodes
-    3. The number of streamlines between the two nodes
-    '''
-    norm = Normalizer()
-    whole = np.zeros((len(get_subj_ids()), tri * 3))
-    #but the matrix is upper triangular so we should only take that into account, then number of features will
-    # get reduced
-    j = 0
-    for subject in get_subj_ids():
-        out_diff = f'/data/skapoor/HCP/results/{subject}/T1w/Diffusion'
-        files = [f'{out_diff}/mean_FA_connectome_1M_SIFT.csv', f'{out_diff}/distances_mean_1M_SIFT.csv',
-                 f'{out_diff}/connectome_1M.csv']
-        i = 0
-        for file in files:
-            #print(file) # we need to make all edges as a feature for each subject!
-            # so we will have 84x84 features
-            edge_feature = np.array(pd.read_csv(file, sep =' ', header= None))
-            # the file shall be number of subjects x 7056
-            edge_feature = edge_feature[np.triu_indices(84)]
-            whole[j,i*tri:(i+1)*tri] = edge_feature
-            #print(i,j)
-            i+=1
-        j+=1
-    '''we need to normalise the data since the scales are different 
-    and we still want to retain the variance
-    '''
-    whole = norm.fit_transform(whole)
-    return whole
-#%%
-def hist_correlation(data, whole, labels, edge_names, big5, tri):
-    fig, ax = plt.subplots(5,3, figsize =(10,10))
-    for j in range(len(labels)):
-        label = np.array(data[labels[j]]).reshape(-1,1)
-        # correlation of mean FA edges, mean str length, number of strl with Openness
-        for i in range(3):
-            map_o = np.concatenate((whole[:, i*tri:(i+1)*tri], label),axis =1)
-            corr = np.cov(map_o, rowvar=False)
-            ax[j][i].hist(corr[-1][:-1], log = True, bins=100)
-            ax[j][i].set_title(big5[j]+' '+ edge_names[i])
-            ax[j][i].set_ylabel('Num edges')
-            ax[j][i].set_xlabel('Correlation coeff')
-    plt.savefig('reports/correlation_distribution.png')
-    plt.show()
-
-def hist_fscore(whole, labels, big5, edge_names, tri):
-    #to return the fscore in order to get the best performing features according to fscore
-    fscores = np.zeros((5,3, tri))
-    fig, ax = plt.subplots(5,3, figsize = (15,15))
-    for j in range(len(labels)):
-        # thresholding for converting the data to binary format for classification
-        bin_label = data[labels[j]] >= data[labels[j]].median()
-        bin_label = bin_label.astype(int)
-        bin_label.reset_index(drop=True, inplace=True)
-        for i in range(3):
-                data_edges = pd.DataFrame(whole[:, i *tri:(i + 1) * tri])
-                data_edges.reset_index(inplace=True, drop= True)
-                #print(bin_label.head())
-                #print('x1', data[label])
-                assert len(data_edges)== len(data[labels[j]])
-
-                df = pd.concat([data_edges, bin_label], axis = 1)
-                l1 = list(range(len(data_edges.columns)))
-                l1.append(labels[j])
-                df.columns = l1
-                #print(df.head())
-                #print(fscore(df, label))
-                fscores[j][i] = fscore(df, labels[j])[:-1]
-                if np.isnan(fscores[j][i]).any():
-                    print(j,i)
-                    fscores[j][i][np.isnan(fscores[j][i])] = 0
-                # to do resolve nan values!
-                assert np.isnan(fscores[j][i]).any() == False
-                ax[j][i].hist(np.log1p(fscores[j][i]), log = True, bins=100)
-                ax[j][i].set_title(big5[j] + ' '+edge_names[i])
-                ax[j][i].set_xlabel('F-Score')
-                ax[j][i].set_ylabel('Number of edges')
-    plt.savefig('reports/fscore_distribution.png')
-    plt.show()
-    return fscores
-
+from metrics import compute_scores
+from processing import generate_combined_matrix, hist_fscore, hist_correlation
 #%%
 data = computed_subjects() # labels for the computed subjects
 data.reset_index(inplace= True)
@@ -104,81 +12,58 @@ num = 84
 tri = int(num * (num + 1) * 0.5)
 whole = generate_combined_matrix(tri)
 # Let us take the labels i.e. the ones from unrestricted_files!
-
 #%%
 labels = ['NEOFAC_A', 'NEOFAC_O', 'NEOFAC_C', 'NEOFAC_N', 'NEOFAC_E' ]
 edge_names = ['mean_FA', 'mean strl', 'num streamlines']
 big5 = ['Agreeableness', 'Openness', 'Conscientiousness', 'Neuroticism',
                         'Extraversion']
 #%%
-fscores = hist_fscore(whole, labels,big5, edge_names, tri)
+fscores = hist_fscore(data, whole, labels,big5, edge_names, tri)
 hist_correlation(data, whole, labels, edge_names, big5, tri)
-#%%
-SVM_acc = {}
-RF_acc = {}
-for i in range(5):# different labels
-    print(labels[i], ':', big5[i])
-    SVM_acc[labels[i]] = {}
-    RF_acc[labels[i]] = {}
-    for j in range(3):
-        print(edge_names[j])
-        f_fscores = fscores[i][j] #for the label and the type of edge
-        SVM_acc[labels[i]][edge_names[j]] = {}
-        RF_acc[labels[i]][edge_names[j]] = {}
-        #stats.iqr(f_fscores)
-        for per in [5,10,15,20]:
-            val = np.percentile(f_fscores, 100-per)
-            index = np.where(f_fscores >= val)
-            #print(f'Number of indexes where the values are in the last {per} percentile:', len(index[0]))
-            Y = np.array(data[labels[i]] >= data[labels[i]].median()).astype(int)
-            X = np.reshape(whole, (whole.shape[0], 3,whole.shape[1]//3))[:, j,index[0]]
-            X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.80, test_size=0.2)
-            clf = make_pipeline(Normalizer(), SVC(gamma='auto'))
-            clf.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-            acc = sum(y_pred==y_test)/len(y_test)
-            #print(acc,'SVM', label)
-            SVM_acc[labels[i]][edge_names[j]][per] = acc
-            y_predX, y = make_classification(n_samples= len(data), n_features=len(data.columns),
-                                   n_informative=2, n_redundant=0,
-                                   random_state=0, shuffle=False)
-            clf = RandomForestClassifier(max_depth=2, random_state=0)
-            clf.fit(X_train, y_train)
-            clf.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-            acc = sum(y_pred==y_test)/len(y_test)
-            RF_acc[labels[i]][edge_names[j]][per] = acc
-            #print(acc, 'RF', label)
+
 #%%
 # without taking the edge type into consideration
 new_fscores = np.reshape(fscores, (fscores.shape[0], fscores.shape[1]*fscores.shape[2]))
-SVM_acc_2 = {}
-RF_acc_2 = {}
+metrics = ['balanced accuracy', 'AUC', 'accuracy', 'F1 score']
+SVM_score = {}
+RF_score = {}
 for i in range(5):# different labels
     print(labels[i], ':', big5[i])
-    SVM_acc_2[big5[i]] = {}
-    RF_acc_2[big5[i]] = {}
+    SVM_score[big5[i]] = {}
+    RF_score[big5[i]] = {}
     #stats.iqr(f_fscores)
     for per in [5,10,15,20]:
+            SVM_score[big5[i]][per] = {}
+            RF_score[big5[i]][per] = {}
             val = np.percentile(new_fscores[i], 100-per)
             index = np.where(new_fscores[i] >= val)
             #print(f'Number of indexes where the values are in the last {per} percentile:', len(index[0]))
             Y = np.array(data[labels[i]] >= data[labels[i]].median()).astype(int)
             X = whole[:,index[0]]
-            X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.90, test_size=0.1)
-            clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
-            clf.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-            acc = sum(y_pred==y_test)/len(y_test)
-            #print(acc,'SVM', label)
-            SVM_acc_2[big5[i]][per] = acc
-            y_predX, y = make_classification(n_samples= len(data), n_features=len(data.columns),
-                                   n_informative=2, n_redundant=0,
-                                   random_state=0, shuffle=False)
-            clf = RandomForestClassifier(max_depth=2, random_state=0)
-            clf.fit(X_train, y_train)
-            clf.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-            acc = sum(y_pred==y_test)/len(y_test)
-            RF_acc_2[big5[i]][per] = acc
+
+            X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.90, test_size=0.1, shuffle=True)
+
+            y_pred, y_score = train_SVC(X_train, y_train, X_test)
+            # according to the documentation the score must be for the higher class
+            scores = compute_scores(y_test, y_pred, y_score)
+            for metric, score in zip(metrics, scores):
+                SVM_score[big5[i]][per][metric] = round(score, 3)
+
+            y_pred, y_score = train_RandomForest(X_train, y_train, y_test)
+            scores = compute_scores(y_test, y_pred, y_score)
+            for metric, score in zip(metrics, scores):
+                RF_score[big5[i]][per][metric] = round(score,3)
             #print(acc, 'RF', label)
+
+#%%
+RF = pd.concat({
+        k: pd.DataFrame.from_dict(v, 'index') for k, v in RF_score.items()
+    },
+    axis=0)
+SVM = pd.concat({
+        k: pd.DataFrame.from_dict(v, 'index') for k, v in SVM_score.items()
+    },
+    axis=0)
+#%%
+RF.to_csv('RF_results.csv')
+SVM.to_csv('SVM_results.csv')
