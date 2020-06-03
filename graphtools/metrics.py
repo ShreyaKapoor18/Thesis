bfb4d56f003fe4a332e8812d602d8e9a2053e929
Shreya Kapoor <kapoorshreya18@gmail.com>
2020-06-03 15:59:46 +0200

    performance measures for algorithm

--- graphtools/metrics.py
+++ graphtools/metrics.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
 #%%
 def f_score(data, feature, label):
     f1 = data.loc[data.loc[:,label]==0,feature]
@@ -39,3 +43,8 @@ def fscore(data, class_col='class'):
         return round(numerator/denominator, 3)
     else:
         return np.zeros(numerator.shape)
+
+def compute_scores(y_test, y_pred, y_score):
+    scores = [balanced_accuracy_score(y_test, y_pred), roc_auc_score(y_test, y_score),
+              accuracy_score(y_test, y_pred), f1_score(y_test, y_pred)]
+    return scores
