2517baa39963ca8268d068bf07ac09c01ee09ba2
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2020-07-06 12:30:17 +0200

    use individual features; FA,meanstrl, num strl

--- graphtools/graph_differences.py
+++ graphtools/graph_differences.py
@@ -11,14 +11,16 @@ We make one networkx Graph for all sub
         iii. Random Forest score of the edge
 Store the values in the a json dictionary
 """
-import os
-import numpy as np
 import json
+import os
+
+import networkx as nx
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from classification import data_splitting
-import networkx as nx
 from sklearn.preprocessing import scale
+
+from classification import data_splitting
 
 
 # %%
@@ -61,77 +63,89 @@ def different_graphs(fscores, mat, big5, data, whole, labels, corr, mews):
             # Let's say we only choose the throw median choice, because it is the one that makes more sense
             choice = 'throw median'  # out of all these we will use these particular choices only!
             X, y = data_splitting(choice, i, index, data, whole, labels)  # this X is for random forests training
-            params = best_params['RF'][big5[i]]["5"][choice]#maybe use the parameters that work the best for top 5%
+            params = best_params['RF'][big5[i]]["100"][choice]  # maybe use the parameters that work the best for top 5%
             feature_imp = train_with_best_params('RF', params, X, y)
             feature_imp = np.reshape(feature_imp, (3, feature_imp.shape[0] // 3))
 
             # edges = set()
-            edge_attributes = []
+
             # node_attributes = {x: random.randint(-10, 10) for x in range(84)}
             for edge, arr in zip(['fscores', 'pearson', 'feature_importance'],
                                  [fscores[i, :, :], corr[i, :, :], feature_imp]):
                 # for each edge type we have a different feature
 
-                thresh = np.percentile(np.absolute(arr), 20)  # remove bottom 20 percentile
+                thresh = np.percentile(np.absolute(arr), 10)  # remove bottom 5% in absolute terms
                 index2 = np.where(np.absolute(arr) < thresh)
                 print('indexes', index2)
                 arr[index2[0], index2[1]] = 0
-                for j in range(len(mat[0])):
-                    edge_attributes.append((mat[0][j], mat[1][j], np.mean(arr[:, j])))
-                g1 = nx.Graph()
-                g1.add_nodes_from(range(84))
-                # g1.add_edges_from(edges)
-                #print(g1.nodes)
-                g1.add_weighted_edges_from(edge_attributes)  # shall be a list of tuples
 
-                for node_wts in ['const', 'max']:
-                    node_labels = []
-                    for l in range(len(g1.nodes)):
-                        if node_wts == 'max':
-                            g1.nodes[l]['label'] = max([g1[l][k]['weight'] for k in range(len(g1[l]))])
-                        elif node_wts == 'const':
-                            g1.nodes[l]['label'] = 1
-                        node_labels.append(g1.nodes[l]['label'])
-                    node_labels = scale(node_labels)  # standardizing the node labels
-                    for l in range(len(g1.nodes)):
-                        g1.nodes[l]['label'] = node_labels[l]
+                    # try for for different types, one feature at a time maybe and then construct graph?
+                for feature in ['mean_FA', 'mean_strl', 'num_str']:
+                    edge_attributes = []
+                    for j in range(len(mat[0])):
+                        if feature == 'mean_FA':
+                            edge_attributes.append((mat[0][j], mat[1][j], arr[0, j]))
+                        if feature == 'mean_strl':
+                            edge_attributes.append((mat[0][j], mat[1][j], arr[1, j]))
+                        if feature == 'num_str':
+                            edge_attributes.append((mat[0][j], mat[1][j], arr[2, j]))
+                    # mean for the scores of three different labels
 
-                    # putting this into the different text files
+                    g1 = nx.Graph()
+                    g1.add_nodes_from(range(84))
+                    # g1.add_edges_from(edges)
+                    # print(g1.nodes)
+                    g1.add_weighted_edges_from(edge_attributes)  # shall be a list of tuples
 
-                    filename = f'{big5[i]}_{edge}_{node_wts}'  # make more nested directories
-                    nodes_file = open(f'{mews}/outputs/nodes/{filename}', 'w')
-                    edges_file = open(f'{mews}/outputs/edges/{filename}', 'w')
-                    print(filename)
-                    count = 0
-                    for x in g1.nodes:
-                        # print(node)
-                        if g1.degree(x) >= 2:
-                            print(str(x) + ' ' * 3 + str(g1.nodes[x]['label']), file=nodes_file)
-                            # print(str(node) + ' ' + str(0), file=nodes_file)
-                            count += 1
+                    for node_wts in ['const', 'max']:
+                        node_labels = []
+                        for l in range(len(g1.nodes)):
+                            if node_wts == 'max':
+                                g1.nodes[l]['label'] = max([g1[l][k]['weight'] for k in range(len(g1[l]))])
+                            elif node_wts == 'const':
+                                g1.nodes[l]['label'] = 1
+                            node_labels.append(g1.nodes[l]['label'])
+                        node_labels = scale(node_labels)  # standardizing the node labels
+                        for l in range(len(g1.nodes)):
+                            g1.nodes[l]['label'] = node_labels[l]
 
-                            # print(node, 'has degree >=2')
-                    print('Number of nodes having degree>=2', count)
-                    # print(len(nodes))
+                        # putting this into the different text files
 
-                    for x in g1.nodes:
-                        # if edge[0] in g1.nodes and edge[1] in g1.nodes:
-                        for conn in g1[x]:
-                            print(str(x) + ' ' * 3 + str(conn) + ' ' * 3 + str(g1[x][conn]['weight']),
-                                  file=edges_file)  # original file format was supposed to have 3 spaces
-                            # print(str(edge[0]) + ' ' + str(edge[1]) + ' ' + str(randint(-5,5)), file = edges_file)
-                    nodes_file.close()
-                    edges_file.close()
+                        filename = f'{big5[i]}_{edge}_{node_wts}_{feature}'  # make more nested directories
+                        nodes_file = open(f'{mews}/outputs/nodes/{filename}', 'w')
+                        edges_file = open(f'{mews}/outputs/edges/{filename}', 'w')
+                        print(filename)
+                        count = 0
+                        for x in g1.nodes:
+                            # print(node)
+                            if g1.degree(x) >= 2:
+                                print(str(x) + ' ' * 3 + str(g1.nodes[x]['label']), file=nodes_file)
+                                # print(str(node) + ' ' + str(0), file=nodes_file)
+                                count += 1
 
-                    print('*' * 100, '\n', filename)
+                                # print(node, 'has degree >=2')
+                        print('Number of nodes having degree>=2', count)
+                        # print(len(nodes))
 
-                    os.chdir(mews)
-                    print('Current directory', os.getcwd())
-                    cmd = (f' java -Xss4M -Djava.library.path=/opt/ibm/ILOG/CPLEX_Studio1210/cplex/bin/x86-64_linux/ '
-                           f'-cp /opt/ibm/ILOG/CPLEX_Studio1210/cplex/lib/cplex.jar:target/gmwcs-solver.jar '
-                           f'ru.ifmo.ctddev.gmwcs.Main -e outputs/edges/{filename} '
-                           f'-n outputs/nodes/{filename} > outputs/solver/{filename}')
-                    print(cmd)
-                    os.system(cmd)
-                    os.chdir("/home/skapoor/Thesis/graphtools")
-                    print('Current directory', os.getcwd())
+                        for x in g1.nodes:
+                            # if edge[0] in g1.nodes and edge[1] in g1.nodes:
+                            for conn in g1[x]:
+                                print(str(x) + ' ' * 3 + str(conn) + ' ' * 3 + str(g1[x][conn]['weight']),
+                                      file=edges_file)  # original file format was supposed to have 3 spaces
+                                # print(str(edge[0]) + ' ' + str(edge[1]) + ' ' + str(randint(-5,5)), file = edges_file)
+                        nodes_file.close()
+                        edges_file.close()
+
+                        print('*' * 100, '\n', filename)
+
+                        os.chdir(mews)
+                        print('Current directory', os.getcwd())
+                        cmd = (
+                            f' java -Xss4M -Djava.library.path=/opt/ibm/ILOG/CPLEX_Studio1210/cplex/bin/x86-64_linux/ '
+                            f'-cp /opt/ibm/ILOG/CPLEX_Studio1210/cplex/lib/cplex.jar:target/gmwcs-solver.jar '
+                            f'ru.ifmo.ctddev.gmwcs.Main -e outputs/edges/{filename} '
+                            f'-n outputs/nodes/{filename} > outputs/solver/{filename}')
+                        print(cmd)
+                        os.system(cmd)
+                        os.chdir("/home/skapoor/Thesis/graphtools")
+                        print('Current directory', os.getcwd())
--- graphtools/read_graphs.py
+++ graphtools/read_graphs.py
@@ -1,10 +1,14 @@
+import os
 import numpy as np
-from processing import generate_combined_matrix
-from readfiles import computed_subjects
+from sklearn.model_selection import RandomizedSearchCV
 from classification import data_splitting
 from paramopt import get_distributions
-from sklearn.model_selection import RandomizedSearchCV
-
+from processing import generate_combined_matrix
+from readfiles import computed_subjects
+#%%
+#just for the time being
+import warnings
+warnings.filterwarnings("ignore")
 # %%
 mat = np.triu_indices(84)
 mews = '/home/skapoor/Thesis/gmwcs-solver'
@@ -22,44 +26,57 @@ assert list(whole.index) == list(data.index)
 for i in range(5):  # different labels
     for edge in ['fscores', 'pearson', 'feature_importance']:
         for node_wts in ['const', 'max']:
-            filename = f'{big5[i]}_{edge}_{node_wts}'  # make more nested directories
-            print(filename + '.out')
-            with open(f'{mews}/outputs/nodes/{filename}.out', 'r') as nodes_file, \
-                    open(f'{mews}/outputs/edges/{filename}.out', 'r') as edges_file:
-                nodes = [x.split('\t') for x in nodes_file.read().split('\n')]
-                edges = [x.split('\t') for x in edges_file.read().split('\n')]
+            for feature in ['mean_FA', 'mean_strl', 'num_str']:
+                filename = f'{big5[i]}_{edge}_{node_wts}_{feature}'  # make more nested directories
+                print(filename + '.out')
+                if os.path.exists(f'{mews}/outputs/nodes/{filename}.out') \
+                        and os.path.exists(f'{mews}/outputs/edges/{filename}.out'):
+                    with open(f'{mews}/outputs/nodes/{filename}.out', 'r') as nodes_file, \
+                            open(f'{mews}/outputs/edges/{filename}.out', 'r') as edges_file:
+                        nodes = [x.split('\t') for x in nodes_file.read().split('\n')]
+                        edges = [x.split('\t') for x in edges_file.read().split('\n')]
 
-                nodes_e = set()
-                edges_e = set()
-                for a in nodes[:-1]:
-                    if a[1] != 'n/a':
-                        nodes_e.add(int(a[0]))
-                feature_indices = set()
-                all_feature_indices = []
-                # 0 to range(len(mat)), everything in matrix whole corresponding to this edge is feature
-                for existing_edge in edges[:-1]:
-                    if existing_edge[-1] != 'n/a':
-                        edges_e.add((int(existing_edge[0]), int(existing_edge[1])))
-                        for k in range(len(mat[0])):
-                            if (int(existing_edge[0]), int(existing_edge[1])) == (mat[0][k], mat[1][k]):
-                                feature_indices.add(k)
-                                # all_feature_indices.extend([k, k+tri, k+2*tri]) # for the three types FA, n strl, strlen
-                                all_feature_indices.extend([k, k + tri, k + 2 * tri])
-                # print('Existing nodes', nodes_e)
-                # print('Existing edges', edges_e)
-                # print('Feature indexes, edge indexes', feature_indices)
-                print('*' * 100)
-                # train classifier based on these edge indices
-                for choice in ['qcut', 'median', 'throw median']:
-                    if all_feature_indices != []:
-                        X, y = data_splitting(choice, i, all_feature_indices, data, whole, labels)
-                        for classifier in ['SVC', 'RF', 'MLP']:
-                            print(choice, classifier)
-                            clf, distributions = get_distributions(classifier)
-                            rcv = RandomizedSearchCV(clf, distributions, random_state=55, scoring=metrics,
-                                                     refit='roc_auc_ovr_weighted', cv=5)
-                            # scores = cross_validate(clf, X, Y, cv=5, scoring=metrics)
-                            search = rcv.fit(X, y)
-                            scores = search.cv_results_
-                            for metric in metrics:
-                                print(f'mean_test_{metric}:', round(np.mean(scores[f'mean_test_{metric}']), 3))
+                        nodes_e = set()
+                        edges_e = set()
+                        for a in nodes[:-1]:
+                            if a[1] != 'n/a':
+                                nodes_e.add(int(a[0]))
+                        feature_indices = set()
+                        all_feature_indices = []
+                        # 0 to range(len(mat)), everything in matrix whole corresponding to this edge is feature
+                        for existing_edge in edges[:-1]:
+                            if existing_edge[-1] != 'n/a':
+                                edges_e.add((int(existing_edge[0]), int(existing_edge[1])))
+                                for k in range(len(mat[0])):
+                                    if (int(existing_edge[0]), int(existing_edge[1])) == (mat[0][k], mat[1][k]):
+                                        feature_indices.add(k)
+                                        # all_feature_indices.extend([k, k+tri, k+2*tri]) # for the three types FA, n strl, strlen
+                                        #all_feature_indices.extend([k, k + tri, k + 2 * tri])
+                                        if feature == 'mean_FA':
+                                            all_feature_indices.append(k)
+                                            feature_mat = whole.iloc[:, :tri]
+                                        if feature == 'mean_strl':
+                                            all_feature_indices.append(k+tri)
+                                            feature_mat = whole.iloc[:, tri:2*tri]
+                                        if feature == 'num_str':
+                                            all_feature_indices.append(k+2*tri)
+                                            feature_mat = whole.iloc[:, 2*tri:]
+                        # print('Existing nodes', nodes_e)
+                        # print('Existing edges', edges_e)
+                        # print('Feature indexes, edge indexes', feature_indices)
+                        print('*' * 100)
+                        # train classifier based on these edge indices
+                        print('Number of features selected by the solver', len(all_feature_indices))
+                        for choice in ['throw median']:
+                            if all_feature_indices != []:
+                                X, y = data_splitting(choice, i, all_feature_indices, data, feature_mat, labels)
+                                for classifier in ['SVC', 'RF', 'MLP']:
+                                    print(choice, classifier)
+                                    clf, distributions = get_distributions(classifier)
+                                    rcv = RandomizedSearchCV(clf, distributions, random_state=55, scoring=metrics,
+                                                             refit='roc_auc_ovr_weighted', cv=5)
+                                    # scores = cross_validate(clf, X, Y, cv=5, scoring=metrics)
+                                    search = rcv.fit(X, y)
+                                    scores = search.cv_results_
+                                    for metric in metrics:
+                                        print(f'mean_test_{metric}:', round(np.mean(scores[f'mean_test_{metric}']), 3))
