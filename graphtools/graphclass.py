d3cc99e16c719e1b54a361f4a9898a09119f11ed
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2020-08-04 18:36:32 +0200

    OOP for subgraph class

---
+++ graphtools/graphclass.py
@@ -0,0 +1,127 @@
+import os
+import networkx as nx
+from sklearn.preprocessing import scale
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.cm as cm
+import numpy as np
+
+class Graph(nx.Graph): #inheriting from networkx graph package along with the functionality we want
+    def __init__(self, edge, feature_type, node_wts, target):
+        self.edge = edge
+        self.feature_type = feature_type
+        self.node_wts = node_wts
+        self.target = target
+        self.filename = f'{target}_{edge}_{node_wts}_{feature_type}'
+    def set_edge_labels(self, edge_attributes): #maybe this function is not even needed
+        self.add_weighted_edges_from(edge_attributes)
+
+    def set_node_labels(self, node_labels, node_wts):
+        node_labels = []
+        for l in self.nodes.keys():
+            if node_wts == 'max':
+                self.nodes[l]['label'] = max([[l][k]['weight'] for k in self[l].keys()])  # max or max abs?
+            elif node_wts == 'const':
+                self.nodes[l]['label'] = 1
+            node_labels.append(self.nodes[l]['label'])
+        self.node_labels = node_labels
+
+    def normalize_node_attr(self):
+        self.node_labels = scale(self.node_labels)  # standardizing the node labels
+        for n in self.nodes.keys():
+            self.nodes[n]['label'] = self.node_labels[n]
+
+    def visualize_graph(self, mews, connected, threshold, plotting_options):
+
+        edge_wts = []
+        for m in self.connected_subgraph.edges.data():
+            # print(m)
+            edge_wts.append(m[2]['weight'])
+
+        minima = min(edge_wts)
+        maxima = max(edge_wts)
+        norm = matplotlib.colors.Normalize(vmin=minima, vmax=maxima)
+        mapper = cm.ScalarMappable(norm=norm, cmap=plt.get_cmap('Spectral'))
+        color = []
+
+        for v in edge_wts:
+            color.append(mapper.to_rgba(v))
+        plt.figure()
+
+        if connected:  # to visualize the connected subgraph in the input
+            plt.title(f'Nodes with degree >={self.connected_subgraph.degree}, input to the solver: {self.filename}\n Number of edges {len(self.edges)}\n'
+                      f'Features above percentile: {threshold}, Target: {self.target}, Feature:{self.feature_type}\n'
+                      f'Edge type:{self.edge}, Node weighting:{self.node_wts}')
+            nx.draw(self.connected_subgraph, **plotting_options, edge_color=color)
+            plt.savefig(f'{mews}/outputs/figs/{self.filename}.png')
+
+        else:
+            plt.title(f'Output from the solver: {self.filename}\n Number of edges {len(self.edges)}\n'
+                      f'Features above percentile: {threshold}, Target: {self.target}, Feature:{self.feature_type}\n'
+                      f'Edge type:{self.edge}, Node weighting:{self.node_wts}')
+            nx.draw(self, **plotting_options, edge_color=color)
+            plt.savefig(f'{mews}/outputs/figs/{self.filename}_out.png')
+        plt.show()
+
+    def savefiles(self, mews, degree):
+
+        connected_nodes = []
+        count = 0
+        with open(f'{mews}/outputs/nodes/{self.filename}', 'w') as nodes_file:
+            for x in self.nodes:
+                # print(node)
+                if self.degree(x) >= degree:  # solver documentation, 1 or 2
+                    print(str(x) + ' ' * 3 + str(self.nodes[x]['label']), file=nodes_file)
+                    connected_nodes.append(x)
+                    # print(str(node) + ' ' + str(0), file=nodes_file)
+                    count += 1
+        self.connected_subgraph = self.subgraph(connected_nodes)
+        self.connected_subgraph.degree = degree
+
+        with open(f'{mews}/outputs/edges/{self.filename}', 'w') as edges_file:
+            for x in self.nodes:
+                # if edge[0] in g1.nodes and edge[1] in g1.nodes:
+                for conn in self[x]:
+                    print(str(x) + ' ' * 3 + str(conn) + ' ' * 3 + str(self[x][conn]['weight']),
+                          file=edges_file)  # original file format was supposed to have 3 spaces
+
+    def run_solver(self, mews):
+        os.chdir(mews)
+        print('Current directory', os.getcwd())
+        cmd = (
+            f' java -Xss4M -Djava.library.path=/opt/ibm/ILOG/CPLEX_Studio1210/cplex/bin/x86-64_linux/ '
+            f'-cp /opt/ibm/ILOG/CPLEX_Studio1210/cplex/lib/cplex.jar:target/gmwcs-solver.jar '
+            f'ru.ifmo.ctddev.gmwcs.Main -e outputs/edges/{self.filename} '
+            f'-n outputs/nodes/{self.filename} > outputs/solver/{self.filename}') #training data into the solver
+        print(cmd)
+        os.system(cmd)
+        os.chdir("/home/skapoor/Thesis/graphtools")
+        print('Current directory', os.getcwd())
+
+    def read_from_file(self, mews, mat=np.triu(84)):
+        if os.path.exists(f'{mews}/outputs/nodes/{self.filename}.out') \
+                and os.path.exists(f'{mews}/outputs/edges/{self.filename}.out'):
+            with open(f'{mews}/outputs/nodes/{self.filename}.out', 'r') as nodes_file, \
+                    open(f'{mews}/outputs/edges/{self.filename}.out', 'r') as edges_file:
+                nodes = [x.split('\t') for x in nodes_file.read().split('\n')]
+                edges = [x.split('\t') for x in edges_file.read().split('\n')]
+                nodes_e = set()
+                edges_e = set()
+                for a in nodes[:-1]:
+                    if a[1] != 'n/a': # since the last line is the subnet score
+                        nodes_e.add(int(a[0]))
+                feature_indices = []
+
+                # 0 to range(len(mat)), everything in matrix whole corresponding to this edge is feature
+                for existing_edge in edges[:-1]:
+                    if existing_edge[-1] != 'n/a':
+                        edges_e.add((int(existing_edge[0]), int(existing_edge[1]), float(existing_edge[2])))
+                        for k in range(len(mat[0])):
+                            if (int(existing_edge[0]), int(existing_edge[1])) == (mat[0][k], mat[1][k]):
+                                # all_feature_indices.extend([k, k+tri, k+2*tri]) # for the three types FA, n strl, strlen
+                                # all_feature_indices.extend([k, k + tri, k + 2 * tri])
+                                feature_indices.append(k)
+                                # feature_mat = whole.iloc[:, :tri]]
+                self.add_nodes_from(nodes_e)
+                self.set_edge_labels(edges_e)
+                self.set_node_labels([node[1] for node in nodes[:-1] if int(node[0]) in nodes_e])
--- graphtools/subgraphclass.py
+++ graphtools/subgraphclass.py
@@ -4,9 +4,8 @@ from readfiles import computed_subjects
 from read_graphs import train_from_reduced_graph
 from classification import run_classification
 from paramopt import graph_options
-from sklearn.model_selection import train_test_split
 # %%
-'''Data computed for all 5 personality traits at once'''
+''' Data computed for all 5 personality traits at once'''
 data = computed_subjects()  # labels for the computed subjects, data.index is the subject id
 num = 84  # number of nodes in the graph
 tri = int(num * (num + 1) * 0.5)  # we want only the upper diagonal parts since everything below diagonal is 0
@@ -20,18 +19,29 @@ mapping = {k: v for k, v in zip(big5, labels)}
 mat = np.triu_indices(84)
 mews = '/home/skapoor/Thesis/gmwcs-solver'
 # before
+# this is what is supposed to be done
 metrics = ['balanced_accuracy', 'accuracy', 'f1_weighted', 'roc_auc_ovr_weighted']
-
-#for the classification step
-target = input('Enter the label you want to be classified')
-target_col = data[mapping[target]]
-feature_type = input('Enter the feature type you want for the graph')
+#note: right now the matrix whole is not scaled, for computing the fscores and correlation coeff it has to be so.
+fscores = hist_fscore(data, whole, labels, big5, edge_names, tri)
+corr = hist_correlation(data, whole, labels, edge_names, big5, tri)
 feature_type = 'mean_FA'
-if feature_type == 'mean_FA':
-    whole = whole.iloc[:, :tri]
-elif feature_type == 'mean_strl':
-    whole = whole.iloc[:, tri:2*tri]
-elif feature_type == 'num_streamlines':
-    whole = whole.iloc[:, 2*tri:]  #input one feature at a time
-# The labels i.e. the ones from unrestricted_files # the order in which the subjects
+target = 'Agreeableness'
+target_col = data[mapping[target]]
+#%%
 
+nested_outputdirs(mews='/home/skapoor/Thesis/gmwcs-solver')
+with open('outputs/combined_params.json', 'r') as f:
+    best_params = json.load(f)
+    i = big5.index(target)
+    index = list(range(tri))  # since we are using one feature at a time
+    # for choice in ['qcut', 'median', 'throw median']
+    # Let's say we only choose the throw median choice, because it is the one that makes more sense
+    choice = 'throw median'  # out of all these we will use these particular choices only!
+    X, y = data_splitting(choice, index, whole, target_col)  # this X is for random forests training
+    params = best_params['RF'][target]["100"][choice]  # maybe use the parameters that work the best for top 5%
+    feature_imp = train_with_best_params('RF', params, X, y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y)
+    #train a graph based only on the training set
+    #get fscores for all the features, only based on the training set.
+    # maybe I can make a graph class based on this and initialize on the basis of the properties
