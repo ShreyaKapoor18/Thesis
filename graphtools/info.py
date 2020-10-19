d9f8bacb58a7c22236a5631f6d402c3f7d1f6047
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2020-10-19 15:22:59 +0200

    Figures for comparing the baseline and solver experiments

--- graphtools/info.py
+++ graphtools/info.py
@@ -1,4 +1,4 @@
-
+'''
 import pandas as pd
 import copy
 from itertools import product
@@ -8,7 +8,7 @@ from readfiles import *
 from decision import filter_summary
 from subgraphclass import make_solver_summary
 from metrics import *
-''' Data computed for all 5 personality traits at once'''
+ Data computed for all 5 personality traits at once
 # labels for the computed subjects, data.index is the subject id
 num = 84  # number of nodes in the graph
 tri = int(num * (num + 1) * 0.5)  # we want only the upper diagonal parts since everything below diagonal is 0
@@ -35,3 +35,82 @@ mean_FA.iloc[:, diag_flattened_indices(84)].mean()
 mean_FA.iloc[:, set(range(3570)).difference(set(diag_flattened_indices(84)))].mean().mean()
 mean_FA.max().max()
 mean_FA.mean().max()
+#%%
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('outputs/csvs/summary.csv')
+x = df['Output_Graph_nodes']
+y = df['Output_Graph_edges']
+target = 'Gender'
+m, b = np.polyfit(x,y,1)
+plt.plot(x, m*x +b)
+plt.scatter(x,y)
+plt.xlabel('Number of nodes preserved')
+plt.ylabel('Number of edges preseved')
+plt.title(f'y = {m.round(3)} x + {b.round(3)}')
+plt.savefig(f'outputs/figures/{target}_nodes_preserved.png')
+#%%
+solver = pd.read_csv('outputs/csvs/solver.csv')
+s1 = solver.groupby(by=['Type of feature', 'Edge'])
+
+def multi_plot(x, y1,y2, y3, y4, **kwargs):
+    plt.ylabel('metrics')
+    plt.scatter(x, y1, c='red', label = 'test_roc_auc_ovr_weighted',alpha =0.5 )
+    plt.scatter(x,y2, c='blue', label = 'test_accuracy', alpha =0.5 )
+    plt.scatter(x,y3, c='pink', label= 'test_balanced_accuracy', alpha =0.5 )
+    plt.scatter(x,y4, c='orange', label= 'test_f1_weighted', alpha =0.5)
+    plt.legend()
+
+
+#%%
+fig = plt.figure(figsize=(50,40))
+g = sns.FacetGrid(solver, col='Type of feature', row ='Edge', sharex=True, margin_titles=True,
+                  legend_out=True,hue_kws=dict(color=['red','blue','pink','orange', "1","2","3","4"]))
+g= (g.map(multi_plot, 'Num edges','test_roc_auc_ovr_weighted', 'test_accuracy',
+      'test_balanced_accuracy', 'test_f1_weighted')).add_legend()
+#[plt.setp(ax.texts, text="") for ax in g.axes.flat]
+# remove the original texts
+                                                    # important to add this before setting titles
+#g.set_titles(row_template = '{row_name}', col_template = '{col_name}')
+g.axes[0,0].set_ylabel('metrics')
+g.axes[1,0].set_ylabel('metrics')
+fig.suptitle('Gender Classification - Solver')
+plt.savefig('outputs/figures/solver_results.png')
+plt.show()
+#%%
+base=pd.read_csv('outputs/csvs/base.csv')
+base = base[base['Num_features']<=1000]
+fig = plt.figure(figsize=(50,40))
+g = sns.FacetGrid(base, col='Type of feature', row ='Edge', sharex=True, margin_titles=True,
+                  legend_out=True,hue_kws=dict(color=['red','blue','pink','orange', "1","2","3","4"]))
+g= (g.map(multi_plot, 'Num_features','test_roc_auc_ovr_weighted', 'test_accuracy',
+      'test_balanced_accuracy', 'test_f1_weighted')).add_legend()
+#[plt.setp(ax.texts, text="") for ax in g.axes.flat]
+# remove the original texts
+                                                    # important to add this before setting titles
+#g.set_titles(row_template = '{row_name}', col_template = '{col_name}')
+g.axes[0,0].set_ylabel('metrics')
+g.axes[1,0].set_ylabel('metrics')
+fig.suptitle('Gender classification- Baseline')
+plt.savefig('outputs/figures/baseline_results.png')
+plt.show()
+#%%
+fig = plt.figure(figsize=(50,40))
+g = sns.FacetGrid(solver, col='Type of feature', row ='Classifier', sharex=True, margin_titles=True,
+                  legend_out=True,hue_kws=dict(color=['red','blue','pink','orange', "1","2","3","4"]))
+g= (g.map(multi_plot, 'Num edges','test_roc_auc_ovr_weighted', 'test_accuracy',
+      'test_balanced_accuracy', 'test_f1_weighted')).add_legend()
+#[plt.setp(ax.texts, text="") for ax in g.axes.flat]
+# remove the original texts
+                                                    # important to add this before setting titles
+#g.set_titles(row_template = '{row_name}', col_template = '{col_name}')
+g.axes[0,0].set_ylabel('metrics')
+g.axes[1,0].set_ylabel('metrics')
+fig.suptitle('Gender Classification - Solver')
+plt.savefig('outputs/figures/solver_results_classifier.png')
+plt.show()
