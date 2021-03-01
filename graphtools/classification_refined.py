69151a9122b97af114a39769f288a8bd9dc5915d
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2021-03-01 15:18:29 +0100

    Openness greedy results

--- graphtools/classification_refined.py
+++ graphtools/classification_refined.py
@@ -53,7 +53,9 @@ def process_raw(X_train, X_test, y_train, edge):
             #arr.append((-1)*np.log10(ttest_ind(group0.iloc[:, i], group1.iloc[:, i]).pvalue))
             arr.append(abs(ttest_ind(group0.iloc[:, i], group1.iloc[:, i]).statistic))
     arr = pd.DataFrame(arr)
+    
     arr.fillna(0, inplace=True)
+    
     return X_train, X_test, arr
 
 
